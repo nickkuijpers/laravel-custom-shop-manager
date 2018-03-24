9375659b672b70ec26138dd97d8dfc2ba97016fd
Nick Kuijpers <info@niku-solutions.nl>
2018-03-24 14:15:36 +0100

    t

--- config/niku-cart.php
+++ config/niku-cart.php
@@ -2,15 +2,15 @@
 
 return [
 
-    'templates' => [        
+    'templates' => [
         'standaard' => [
             'label' => 'Standaard product',
-            'class' => App\Cart\Templates\Simple::class,
+            'class' => App\Application\Custom\Cart\Templates\Simple::class,
         ],
         'licentie' => [
             'label' => 'Licentie product',
-            'class' => App\Cart\Templates\Complex::class,
+            'class' => App\Application\Custom\Cart\Templates\Complex::class,
         ]
-    ],    
+    ],
 
 ];
